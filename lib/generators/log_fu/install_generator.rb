34e945ee35dc3d07db4bcdd302ca3637fd0767f6
Alex <blackanger.z@gmail.com>
2011-05-18 22:37:04 +0800

    delete

--- lib/generators/log_fu/install_generator.rb
+++ lib/generators/log_fu/install_generator.rb
@@ -12,19 +12,5 @@ module LogFu
       end
     end
 
-    class Rails::MongoidModelGenerator < Rails::Generators::NamedBase
-      def create_mongoid_model_file
-        create_file "app/models/#{file_name}.rb", <<-FILE
-        class #{class_name}
-          include LogFu::LogGeneral::MongoIdModels
-          # you need add logged it method
-          # example:
-          # define_logged_it_methods :subject
-          # TODO
-        end
-        FILE
-      end
-    end
-
   end
 end
